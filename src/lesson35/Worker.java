41464569d9a76c4a9c6f46ffcd773f67ea123366
NazarMh <nazar.myhailiv@gmail.com>
2018-07-18 20:48:06 +0300

    Created new class Worker

---
+++ src/lesson35/Worker.java
@@ -0,0 +1,34 @@
+package lesson35;
+
+public class Worker {
+    private int id;
+    private String name;
+    private int salary;
+
+    public Worker() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
